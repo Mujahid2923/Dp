64ea5f6fbf4fefdd17908d06e8d9bc30a22ff6ff
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2019-10-29 22:54:42 +0600

    Add files via upload

---
+++ Uva11137.cpp
@@ -0,0 +1,129 @@
+///...................................*****.................................................///
+///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
+///                  Department of Ict                                                      ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+#define          CIN                    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define          ll                     long long int
+#define          ull                    unsigned long long int
+#define          pii                    pair < int, int>
+#define          pll                    pair < ll, ll>
+#define          MOD                    1000000007
+#define          vi                     vector<int>
+#define          vl                     vector<ll>
+#define          pb                     push_back
+#define          sc                     scanf
+#define          pf                     printf
+#define          scin(x)                scanf("%d",&(x))
+#define          scin2(x,y)             scanf("%d %d",&(x),&(y))
+#define          scln(x)                scanf("%lld",&(x))
+#define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
+#define          min3(a,b,c)            min(a,min(b,c))
+#define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
+#define          max3(a,b,c)            max(a,max(b,c))
+#define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
+#define          ms(a,b)                memset(a,b,sizeof(a))
+#define          mp                     make_pair
+#define          gcd(a, b)              __gcd(a,b)
+#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
+#define          input                  freopen("input.txt","rt", stdin)
+#define          output                 freopen("output.txt","wt", stdout)
+#define          PI                     3.141592653589793238462643
+#define          zero(a)                memset(a,0,sizeof a)
+#define          all(v)                 v.begin(),v.end()
+#define          Max(v)                 *max_element(all(v))
+#define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
+#define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define          Unique(X)              (X).erase(unique(all(X)),(X).end())
+#define          no                     cout << "NO" << endl ;
+#define          yes                    cout << "YES" << endl ;
+//#define          endl                   '\n'
+
+///priority_queue<int,vector<int>,greater<int> >pq;
+///string str = "abcdefghijklmnopqrstuvwxyz";
+///string s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+///string s = std::bitset<64>(n).to_string();
+///---------------Order set-------------------
+///template<typename T> using orderset =tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+///#include <ext/pb_ds/assoc_container.hpp>
+///using namespace __gnu_pbds;
+///*os.find_by_order(index) os.order_of_key(value)
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+///sort( all( v ) ) ;
+
+vi v, v1, v2, v3 ;
+vector < pll > vec, vec1, vec2 ;
+vector < pii > adj[ 200000 + 5 ] ;
+map < ll, ll > Mp, Mp2 ;
+set < pii > st, st2 ;
+queue < ll > Q ;
+stack < ll > Stk ;
+multiset < ll > S ;
+multiset < ll > :: iterator it ;
+
+void putting()
+{
+    int i = 1 ;
+    while( i * i * i < 10000 )
+    {
+        v.pb( i * i * i ) ;
+        i ++ ;
+    }
+}
+
+ll DP[ 25 ][ 10005 ] ;
+
+ll Fun( ll pos, ll total )
+{
+    if( pos >= v.size() )
+    {
+        if( total == 0 )
+            return 1 ;
+        else
+            return 0 ;
+    }
+
+    if( DP[ pos ][ total ] != -1 )
+        return DP[ pos ][ total ] ;
+
+    ll r1 = 0 , r2 = 0 ;
+    if( total - v[ pos ] >= 0 )
+    {
+        r1 = Fun( pos , total - v[ pos ] ) ;
+    }
+    r2 = Fun( pos + 1 , total ) ;
+    return DP[ pos ][ total ] = r1 + r2 ;
+}
+
+int main()
+{
+    CIN ;
+    ll n ;
+    memset( DP , -1 , sizeof DP ) ;
+    putting() ;
+    while( cin >> n )
+    {
+        pf( "%lld\n" , Fun( 0 , n ) ) ;
+    }
+
+    return 0 ;
+}
+
+///Always check for n = 1
+
