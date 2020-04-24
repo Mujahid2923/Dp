4967061b493d68b9ed566f3b06d753769d44bb22
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-04-25 01:37:34 +0600

    Update LIS(nlogn).cpp

--- LIS(nlogn).cpp
+++ LIS(nlogn).cpp
@@ -1,32 +1,27 @@
-///...................................*****.................................................///
-///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
-///                  Department of Ict                                                      ///
-///                  Comilla University , Bangladesh.                                       ///
-///...................................*****.................................................///
-
+///Uva481
 #include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
-template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 
 #define          CIN                    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define          ll                     long long int
+#define          ld                     long double
 #define          ull                    unsigned long long int
 #define          pii                    pair < int, int>
 #define          pll                    pair < ll, ll>
 #define          MOD                    1000000007
-#define          vi                     vector<int>
-#define          vl                     vector<ll>
+#define          ff                     first
+#define          ss                     second
 #define          pb                     push_back
+#define          eb                     emplace_back
 #define          sc                     scanf
 #define          pf                     printf
 #define          scin(x)                scanf("%d",&(x))
 #define          scin2(x,y)             scanf("%d %d",&(x),&(y))
+#define          scin3(x,y,z)           scanf("%d %d %d",&(x),&(y),&(z))
 #define          scln(x)                scanf("%lld",&(x))
 #define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
-#define          min3(a,b,c)            min(a,min(b,c))
+#define          scln3(x,y,z)           scanf("%lld %lld %lld",&(x),&(y),&(z))
+#define          min3(a,b,c)            min( { a, b , c } )
 #define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
 #define          max3(a,b,c)            max(a,max(b,c))
 #define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
@@ -36,79 +31,76 @@ template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_
 #define          lcm(a, b)              ((a)*(b)/gcd(a,b))
 #define          input                  freopen("input.txt","rt", stdin)
 #define          output                 freopen("output.txt","wt", stdout)
-#define          PI                     3.141592653589793238462643
+#define          PI                     acos(-1.0)
 #define          zero(a)                memset(a,0,sizeof a)
 #define          all(v)                 v.begin(),v.end()
 #define          Max(v)                 *max_element(all(v))
+#define          Min(v)                 *min_element(all(v))
 #define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
 #define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
 #define          Unique(X)              (X).erase(unique(all(X)),(X).end())
+#define          no                     cout << "NO" << endl ;
+#define          yes                    cout << "YES" << endl ;
+#define          DBG                    cout << endl << "---------------" << endl ;
+#define segment_trele int Lnode = node << 1 , Rnode = Lnode + 1 , mid = ( b + e ) >> 1 ;
 
-///priority_queue<int,vector<int>,greater<int> >pq;
-///string str = "abcdefghijklmnopqrstuvwxyz";
-///string s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+vector < ll > v1, v2, v3 ;
+map < ll, ll > Mp ;
+ll n ;
 
-///---------------Order set-------------------
-///template<typename T> using orderset =tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
-///#include <ext/pb_ds/assoc_container.hpp>
-///using namespace __gnu_pbds;
-///*os.find_by_order(index) os.order_of_key(value)
-
-///--------------Graph Moves--------------------------------------
-///const int fx[] = {+1,-1,+0,+0};
-///const int fy[] = {+0,+0,+1,-1};
-///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
-///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
-///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
-///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
-///---------------------------------------------------------------
-///sort( all( v ) ) ;
-
-vi v1, v2 ;
-
-
-int Fun()
+void Fun()
 {
-    v2.push_back( v1[ 0 ] ) ;
+    ll n = v1.size() ;
+    ll LIS[ n + 2 ] ;
+    LIS[ 0 ] = 0 ;
 
-    for(int i = 1; i < v1.size(); i++)
+    v2.eb( v1[ 0 ] ) ;
+    for( int i = 1 ; i < n ; i++ )
     {
-        if( v2.back() <= v1[ i ] )
+        if( v1[ i ] > v2.back() )
         {
-            v2.push_back( v1[ i ] ) ;
+            LIS[ i ] = v2.size() ;
+            v2.eb( v1[ i ] ) ;
         }
         else
         {
-            int pos = Upper( v2, v1[ i ] ) ;
+            int pos = Lower( v2, v1[ i ] ) ;
+            LIS[ i ] = pos ;
             v2[ pos ] = v1[ i ] ;
         }
     }
-    return ( int ) v2.size() ;
+
+    ll k = v2.size() - 1 ;
+    for( int i = n - 1 ; i >= 0 ; i -- )
+    {
+        if( LIS[ i ] == k )
+        {
+            v3.eb( v1[ i ] ) ;
+            k -- ;
+        }
+    }
 }
 
 int main()
 {
-    CIN ;
-    int n, a ;
-    cin >> n ;
-    for( int i = 0 ; i < n ; i ++ )
+    ll a ;
+    while( scln( a ) != EOF )
     {
-        cin >> a ;
         v1.pb( a ) ;
+        Mp[ a ] = -1 ;
     }
 
-    cout << Fun() << endl ;
+    Fun() ;
 
+    reverse( all( v3 ) ) ;
 
-
+    pf( "%d\n", v3.size() ) ;
+    pf( "-\n" ) ;
+    for( ll x : v3 )
+    {
+        pf( "%lld\n", x ) ;
+    }
     return 0 ;
 }
 
-/*
-16
-0 8 4 12 2 10 6 14 1 9 5 13 3 11 7 15
-ans : 6
-7
-3 4 -1 0 6 2 3
-ans : 4
-*/
+
