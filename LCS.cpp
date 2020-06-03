168a52f8e2aae47b86e4c1860e17474fc05ecd67
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-06-03 23:34:20 +0600

    Update LCS.cpp

--- LCS.cpp
+++ LCS.cpp
@@ -16,6 +16,41 @@ int Fun( int i, int j )
    return dp[ i ][ j ] = ans ;
 }
 
+-----------------------------subsequence print------------------------
+   
+   for( int i = 1 ; i <= n1 ; i ++ )
+    {
+        for( int j = 1 ; j <= n2 ; j ++ )
+        {
+            if( s1[ i - 1 ] == s2[ j - 1 ] )
+            {
+                dp[ i ][ j ] = 1 + dp[ i - 1 ][ j - 1 ] ;
+                sp[ i ][ j ] = sp[ i - 1 ][ j - 1 ] + s1[ i - 1 ] ;
+            }
+            else
+            {
+                if( dp[ i - 1 ][ j ] > dp[ i ][ j - 1 ] )
+                {
+                    dp[ i ][ j ] = dp[ i - 1 ][ j ] ;
+                    sp[ i ][ j ] = sp[ i - 1 ][ j ] ;
+                }
+                else if( dp[ i ][ j - 1 ] > dp[ i - 1 ][ j ] )
+                {
+                    dp[ i ][ j ] = dp[ i ][ j - 1 ] ;
+                    sp[ i ][ j ] = sp[ i ][ j - 1 ] ;
+                }
+                else
+                {
+                    dp[ i ][ j ] = dp[ i ][ j - 1 ] ;
+                    sp[ i ][ j ] = min( sp[ i - 1 ][ j ], sp[ i ][ j - 1 ] ) ;
+                }
+            }
+        }
+    }
+ dp[ n1 ][ n2 ] = length
+ sp[ n1 ][ n2 ] = subsequene
+ 
+ -------------------------------************************---------------------------------
 
 
 ///https://www.spoj.com/problems/CRSCNTRY/en/
