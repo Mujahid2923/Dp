7e51be10ef607ca5ea4380a86e4675faf43d00d2
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-08-31 00:37:08 +0600

    Update Knapsack.cpp

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -1,69 +1,39 @@
-///...................................*****.................................................///
-///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
-///                  Department of Ict                                                      ///
-///                  Comilla University , Bangladesh.                                       ///
-///...................................*****.................................................///
+***************** -------------------------Knapsack Iterative---------------------------------------****************
+    
+#define siz 10005
+int cap, n ;
+int P[ siz ], W[ siz ] ;
+int dp[ siz ][ siz ] ;
 
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
-template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+int main()
+{
+    scin2( cap, n ) ;
+    for( int i = 1 ; i <= n ; i ++ )
+    {
+        scin2( W[ i ], P[ i ] ) ;
+    }
 
-#define          CIN                    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define          ll                     long long int
-#define          ull                    unsigned long long int
-#define          pii                    pair < int, int>
-#define          pll                    pair < ll, ll>
-#define          MOD                    1000000007
-#define          vi                     vector<int>
-#define          vl                     vector<ll>
-#define          pb                     push_back
-#define          sc                     scanf
-#define          pf                     printf
-#define          scin(x)                scanf("%d",&(x))
-#define          scin2(x,y)             scanf("%d %d",&(x),&(y))
-#define          scln(x)                scanf("%lld",&(x))
-#define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
-#define          min3(a,b,c)            min(a,min(b,c))
-#define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
-#define          max3(a,b,c)            max(a,max(b,c))
-#define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
-#define          ms(a,b)                memset(a,b,sizeof(a))
-#define          mp                     make_pair
-#define          gcd(a, b)              __gcd(a,b)
-#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
-#define          input                  freopen("input.txt","rt", stdin)
-#define          output                 freopen("output.txt","wt", stdout)
-#define          PI                     3.141592653589793238462643
-#define          zero(a)                memset(a,0,sizeof a)
-#define          all(v)                 v.begin(),v.end()
-#define          Max(v)                 *max_element(all(v))
-#define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
-#define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
-#define          Unique(X)              (X).erase(unique(all(X)),(X).end())
+    for( int i = 1 ; i <= n ; i ++ )
+    {
+        for( int j = 1 ; j <= cap ; j ++ )
+        {
+            if( j >= W[ i ] )
+            {
+                dp[ i ][ j ] = max( dp[ i - 1 ][ j ] , P[ i ] + dp[ i - 1 ][ j - W[ i ] ] ) ;
+            }
+            else
+            {
+                dp[ i ][ j ] = dp[ i - 1 ][ j ] ;
+            }
+        }
+    }
 
-///priority_queue<int,vector<int>,greater<int> >pq;
-///string str = "abcdefghijklmnopqrstuvwxyz";
-///string s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+    pf( "%d\n", dp[ n ][ cap ] ) ;
+    return 0 ;
+}
 
-///---------------Order set-------------------
-///template<typename T> using orderset =tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
-///#include <ext/pb_ds/assoc_container.hpp>
-///using namespace __gnu_pbds;
-///*os.find_by_order(index) os.order_of_key(value)
 
-///--------------Graph Moves--------------------------------------
-///const int fx[] = {+1,-1,+0,+0};
-///const int fy[] = {+0,+0,+1,-1};
-///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
-///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
-///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
-///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
-///---------------------------------------------------------------
-///sort( all( v ) ) ;
-
+***********************---------------------------------------Knapsack Recursive --------------------------------------------***********************
 vl v ;
 
 int t, n, m, a ;
@@ -123,3 +93,60 @@ int main()
 
     return 0 ;
 }
+
+
+*****************--------------------------------------------- Unbound Khanpsack -------------------------------**************************
+https://www.spoj.com/problems/PIGBANK/en/
+
+
+
+int t, a, b, n, cap ;
+int dp[ 10005 ] ;
+int P[ 505 ], W[ 505 ] ;
+
+int main()
+{
+    scin( t ) ;
+    while( t-- )
+    {
+        scin2( a, b ) ;
+        cap = b - a ;
+        scin( n ) ;
+        for( int i = 1 ; i <= n ; i ++ )
+        {
+            scin2( P[ i ], W[ i ] ) ;
+        }
+
+        for( int i = 1 ; i <= cap ; i ++ )
+        {
+            dp[ i ] = 1e8 ;
+        }
+        for( int i = 1 ; i <= n ; i ++ )
+        {
+            for( int j = 0 ; j <= cap ; j ++ )
+            {
+                if( j >= W[ i ] )
+                {
+                    dp[ j ] = min( dp[ j ], P[ i ] + dp[ j - W[ i ] ] ) ;
+                }
+            }
+        }
+
+        if( dp[ cap ] == 1e8 )
+        {
+            pf( "This is impossible.\n" ) ;
+        }
+        else
+        {
+        pf( "The minimum amount of money in the piggy-bank is %d.\n", dp[ cap ] ) ;
+        }
+    }
+
+    return 0 ;
+}
+
+
+
+
+
+
