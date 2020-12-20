95df8b038b181a2ebfe4d4ab32bd10a9d91f579b
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-12-21 00:39:28 +0600

    Update Regular Expression.cpp

--- Regular Expression.cpp
+++ Regular Expression.cpp
@@ -11,16 +11,16 @@ class Solution {
 public:
     bool isMatch(string text, string pattern) 
     {
-        int dp[ 25 ][ 35 ] = {} ;
+        int dp[ 100 ][ 100 ] = {} ;
         dp[ 0 ][ 0 ] = 1 ;
-        for( int i = 1 ; i <= pattern.size() ; i ++ )
+        for( int i = 1 ; i < pattern.size() ; i ++ )
         {
             if( pattern[ i - 1 ] == '*' ) dp[ 0 ][ i ] = dp[ 0 ][ i - 2 ] ;
         }
         
-        for( int i = 1 ; i <= text.size() ; i ++ )
+        for( int i = 1 ; i < text.size() ; i ++ )
         {
-            for( int j = 1 ; j <= pattern.size() ; j ++ )
+            for( int j = 1 ; j < pattern.size() ; j ++ )
             {
                 if( pattern[ j - 1 ] == '.' || text[ i - 1 ] == pattern[ j - 1 ] ) dp[ i ][ j ] = dp[ i - 1 ][ j - 1 ] ;
                 else if( pattern[ j - 1 ] == '*' ) 
@@ -28,7 +28,7 @@ public:
                     dp[ i ][ j ] = dp[ i ][ j - 2 ] ;
                     if( pattern[ j - 2 ] == '.' || pattern[ j - 2 ] == text[ i - 1 ] )
                     {
-                        dp[ i ][ j ] = dp[ i ][ j ] || dp[ i - 1 ][ j ] ;
+                        dp[ i ][ j ] = dp[ i ][ j ] | dp[ i - 1 ][ j ] ;
                     }
                 }
                 else dp[ i ][ j ] = 0 ;
